0682073f4ac89bde413fd38819232a368f3fb64a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-20 12:38:14 +0300

    add comment to indicate logout user controller function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -169,6 +169,7 @@ export const login = async (req, res) => {
 	}
 };
 
+// Logout user controller function
 export const logout = async (req, res) => {
 	res.clearCookie("token");
 	res.status(200).json({ success: true, message: "Logged out successfully" });
