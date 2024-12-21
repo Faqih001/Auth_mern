a7f85c2e03391f6eab47a0cf27e21c7df61b7826
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-21 23:41:07 +0300

    add comment to indicate error handling in reset password function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -250,6 +250,7 @@ export const resetPassword = async (req, res) => {
 		// return success message
 		res.status(200).json({ success: true, message: "Password reset successful" });
 	} catch (error) {
+		// catch error and return error message
 		console.log("Error in resetPassword ", error);
 		res.status(400).json({ success: false, message: error.message });
 	}
