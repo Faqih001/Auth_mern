8df07292c49dc3cb7464120d1434c744b9135e27
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-23 21:15:11 +0300

    add comments to clarify routes for signup, login, logout, verify-email, forgot-password, and reset-password endpoints

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -16,12 +16,18 @@ const router = express.Router();
 // Define routes for auth endpoints with corresponding controller functions
 router.get("/check-auth", verifyToken, checkAuth);
 
+
 router.post("/signup", signup);
+
 router.post("/login", login);
+
 router.post("/logout", logout);
 
+
 router.post("/verify-email", verifyEmail);
+
 router.post("/forgot-password", forgotPassword);
+
 
 router.post("/reset-password/:token", resetPassword);
 
