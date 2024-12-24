065a38f1b7cc845ac6f38dafc4c76db2a0626c93
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-24 11:58:48 +0300

    add comment to describe LoadingSpinner component in LoadingSpinner.jsx

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 
+// LoadingSpinner component to create a loading spinner
 const LoadingSpinner = () => {
 	return (
 		<div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
